2d7b261ba4552555d7e21bc61db625bb4d29f5e8
atalo <atalopereira01@gmail.com>
2024-08-26 18:41:01 -0300

    ajustado o movimento do cortador

--- levels/scripts/test_level.gd
+++ levels/scripts/test_level.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 func game_over():
-	$HUD.show_game_over()
-	$Cutter.start_pos($StartPositionCutter.position)
+	pass
+	#$HUD.show_game_over()
+	#$Cutter.start_pos($StartPositionCutter.position)
 
 func new_game():
 	await $HUD.show_message("Get Ready!")
